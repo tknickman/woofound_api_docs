ebfd937ccd9d006286aaed23535b58622f2b84ed
Thomas Knickman <tknickman@gmail.com>
2014-01-06 14:40:40 -0500

    Initial Commit
    Working Bootstrap based Sinatra app for docs

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'sinatra'
+
+configure do
+  enable :sessions
+end
+
+
+
+get '/' do
+  erb :guides
+end
+
+get '/examples' do
+  erb :examples
+end
+
+get '/v1' do
+  erb :v1
+end
+
+get '/v2' do
+  erb :v2
+end
+
+get '/console' do
+  erb :console
+end
+
