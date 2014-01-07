6a2ac4c615deef572a45cb755a36a67a0b5a60ae
Thomas Knickman <tknickman@gmail.com>
2014-01-07 12:40:12 -0500

    console javascript updated

--- public/js/woofound-dev.js
+++ public/js/woofound-dev.js
@@ -1,15 +1,38 @@
 
 window.onload = function()
 {
-    document.getElementById("username").onkeyup = function() {
-        document.getElementById("username_curl").innerHTML = this.value;
-};
+    $(document).on('input','#username', function () {
+        $('#username_curl').html(this.value);
+    })
 
-    document.getElementById("password").onkeyup = function() {
-        document.getElementById("password_curl").innerHTML = this.value;
-    };
+    $(document).on('input','#password', function () {
+        $('#password_curl').html(this.value);
+    })
 
+    $(document).on('input','#app_secret', function () {
+        $('#app_secret_curl').html(this.value);
+    })
+
+    $(document).on('input','#id', function () {
+        $('#endpoint_curl').html(this.value);
+    })
+
+
+    $('#dropdown').change(function(){
+        $('#textBoxContainer').empty();
+        var data = $(this).find('option:selected').attr('value');
+        var cleaned_data = data.split("_");
+        var num_args = cleaned_data.length;
+        for (var i = 0; i < num_args; i++){
+            $('#textBoxContainer').append('<label class="control-label" for="textinput">' + cleaned_data[i] + '</label><br/><input id="' + cleaned_data[i] + '" name="textinput" size="25" type="text" placeholder="'+ cleaned_data[i] +'" class="input-xlarge"><br/>');
+        }
+
+    });
 
 
 
 }
+
+
+
+
