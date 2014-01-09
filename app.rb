2dc4a1958557ac0ca58af134e7fbc98ebdd21c7a
Thomas Knickman <tknickman@gmail.com>
2014-01-09 17:26:46 -0500

    Commented and cleaned

--- app.rb
+++ app.rb
@@ -36,66 +36,74 @@ get '/examples' do
   erb :examples
 end
 
-post '/examples' do
-  post = params[:post]
-  @name = post['name']
-
-  puts "HERE "
-
-  erb :guides
-end
-
 get '/v1' do
+  #send in the endpoints data structure so the content can be generated
   @endpoint_data = v1_endpoints
+  #call the view
   erb :v1_new
 end
 
 get '/v2' do
+  #call the view
   erb :v2
 end
 
 get '/console' do
+  #send in the endpoints data structure so the content can be generated
   @endpoint_data = v1_endpoints
+  #call the view
   erb :console
 end
 
 post '/console' do
-
+  #send in the endpoints data structure so the content can be generated
   @endpoint_data = v1_endpoints
+  #get the post response
   response = call_api(params[:post])
 
-
+  #set result to true to trigger our results block in console.erb
   @result = TRUE
+  #send in the the data from the request to the view
   @code = response.code
   @headers = JSON.pretty_generate(JSON.parse(response.headers.to_json))
   @body = JSON.pretty_generate(JSON.parse(response.body))
 
+  #call the view
   erb :console
 end
 
 
 def call_api(post_data)
 
+  #make sure we actually have post data to work with
   unless post_data == nil
+
+    #build the correct url to hit
     v1_url = "https://core.woofound.com"
+    #get the endpoint chosen by the user
     route = post_data['route']
+    #get out the variable parts with a regex
     args_regex = /{\w+}/
     result = post_data['route'].scan(args_regex)
+
+    #use each regex match as the hash key to get out the users submitted variable value
     result.each { |variable|
       route = route.sub(variable, post_data[variable])
     }
-    final_url = v1_url + route
 
+    #build the final url
+    final_url = v1_url + route
 
     begin
       #Build the the authorization header in a new resource
       resource = RestClient::Resource.new(final_url, post_data['username'], post_data['password'])
+      #submit the response and return it
       response = resource.get(:content_type => :json, :accept => :json, 'Woofound-App-Secret' => post_data['app_secret'])
     rescue => error
+      #catch and errors, and return those too (how rest_client works)
       response = error.response
     end
   end
-
 end
 
 
--- public/js/woofound-dev.js
+++ public/js/woofound-dev.js
@@ -28,35 +28,45 @@ window.onload = function()
     $('#main_dropdown').change(function(){
         $('#textBoxContainer').empty();
 
-        //add the universal form boxes (app secret, username, and password)
-        $('#textBoxContainer').append('<label class="control-label" for="textinput">App_Secret</label><br/><input id="app_secret" name="post[app_secret]" size="70" type="text" placeholder="xxxxxxxxxxxx" class="input-xlarge"><br/>');
-        $('#textBoxContainer').append('<label class="control-label" for="textinput">Username</label><br/><input id="username" name="post[username]" size="20" type="text" placeholder="jason@woofound.com" class="input-xlarge"><br/>');
-        $('#textBoxContainer').append('<label class="control-label" for="textinput">Password</label><br/><input id="password" name="post[password]" size="20" type="text" placeholder="password" class="input-xlarge"><br/>');
 
         //get the value and id of the selected element from the dropdown list
         var data = $(this).find('option:selected').attr('value');
         var route = $(this).find('option:selected').attr('id');
 
+        //don't output the default boxes unless an option has actually been selected
+        if (data !=  "blank"){
+            //add the universal form boxes (app secret, username, and password)
+            $('#textBoxContainer').append('<label class="control-label" for="textinput">App_Secret</label><br/><input id="app_secret" name="post[app_secret]" size="70" type="text" placeholder="xxxxxxxxxxxx" class="input-xlarge"><br/>');
+            $('#textBoxContainer').append('<label class="control-label" for="textinput">Username</label><br/><input id="username" name="post[username]" size="20" type="text" placeholder="jason@woofound.com" class="input-xlarge"><br/>');
+            $('#textBoxContainer').append('<label class="control-label" for="textinput">Password</label><br/><input id="password" name="post[password]" size="20" type="text" placeholder="password" class="input-xlarge"><br/>');
+        }
+
+
         //add in a hidden field so we can have access to the chosen route on the backend
         $('#textBoxContainer').append('<input type="hidden" name="post[route]" value="' + route + '">');
 
+        //make sure something was selected
         if (data !=  "None"){
+            //split up the variables (they are joined with & in console.erb)
             var cleaned_data = data.split("&");
             var num_args = cleaned_data.length;
+            //output the correct number of input boxes for the specific route
             for (var i = 0; i < num_args; i++){
+                //build the variable from the data
                 var url_replace = "{" + cleaned_data[i] + "}";
-                console.log(url_replace);
+                //replace the variables in the route with spans so we can live update them
                 route = route.replace(url_replace, '<span id="' + cleaned_data[i] + '_curl" readonly="readonly"> </span>');
-                //add  textbox for each variable
+                //add  the input box for each variable that corresponds to the span in the curl request example
                 $('#textBoxContainer').append('<label class="control-label" for="textinput">' + cleaned_data[i] + '</label><br/><input id="' + cleaned_data[i] + '"name=post[{'+ cleaned_data[i] +'}] size="25" type="text" placeholder="'+ cleaned_data[i] +'" class="input-xlarge"><br/>');
             }
         }
 
+        //add the curl request example
         $('#textBoxContainer').append('<br/><label>Curl:</label><pre>curl -X GET -H "Woofound-App-Secret: <span id="app_secret_curl" readonly="readonly"></span>"  --user "<span id="username_curl" readonly="readonly"></span>:<span id="password_curl" readonly="readonly"> </span>" https://core.woofound.com' + route  + '</pre>');
 
     });
 
-
+    //bind to the form submission button
     $( "#singlebutton" ).bind( "click", function() {
         $( "#console_form" ).submit();
     });
